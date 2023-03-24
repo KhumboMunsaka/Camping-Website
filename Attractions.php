b34ee189d5029ca0137547769f06bce98fdd494a
KhumboMunsaka <khumbolane11@gmail.com>
2023-03-24 15:33:05 +0200

    Added an atttractions page
    I styled them from what I saw on the zambia tourism website

--- Attractions.php
+++ Attractions.php
@@ -4,9 +4,8 @@
         <h2>What To See At Our CampSites</h2>
         <div class="attractions">
 <div class="attraction">
-    <div class="attraction-img">
-        <p>Beatuiful lakes</p>
-        <!-- <img src="https://images.pexels.com/photos/2091351/pexels-photo-2091351.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of a lake"> -->
+    <div class="attraction-img one">
+        <img src="https://images.pexels.com/photos/5620427/pexels-photo-5620427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image of a lake">
     </div>
     <div class="attraction-details">
         <p>The serene lake near our camping site is a natural wonder that never fails to captivate and inspire our visitors. Whether you're looking to swim, fish, paddle, or simply take in the breathtaking scenery, the lake offers a truly picturesque setting that is sure to leave a lasting impression. Come and experience the tranquility and beauty of the lake for yourself, and discover why it is one of the most beloved features of our camping site.</p>
@@ -16,8 +15,18 @@
 
 
 <div class="attraction">
-    <div class="attraction-img">
-        <img src="https://images.pexels.com/photos/3642482/pexels-photo-3642482.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of bike trail">
+    <div class="attraction-img two">
+       <img src="https://images.pexels.com/photos/6271677/pexels-photo-6271677.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image of a campsite">
+    </div>
+    <div class="attraction-details ">
+        <p>Our camp sites are designed to provide the ultimate outdoor experience, with spacious and comfortable accommodations that make you feel right at home in nature. Each site is carefully maintained and thoughtfully equipped with all the amenities you need to enjoy your stay, from fire pits for roasting marshmallows to picnic tables for sharing meals with loved ones. Whether you're a seasoned camper or new to the outdoors, our camp sites offer the perfect balance of comfort and adventure, making for an unforgettable experience you'll treasure for years to come.</p>
+        <button>Learn More</button>
+    </div>
+</div>
+
+<div class="attraction">
+    <div class="attraction-img three">
+        <img src="https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of a biker">
     </div>
     <div class="attraction-details">
         <p>The bike trail near our camping site is a true gem for outdoor enthusiasts. It winds through stunning landscapes and offers breathtaking views that will take your breath away. Whether you're a seasoned biker or just looking for a leisurely ride, the trail is perfect for all skill levels. Take a break from the hustle and bustle of everyday life and let the beauty of nature and the thrill of biking invigorate your senses.</p>
@@ -26,18 +35,8 @@
 </div>
 
 <div class="attraction">
-    <div class="attraction-img">
-        <img src="https://images.pexels.com/photos/9899941/pexels-photo-9899941.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of camp site">
-    </div>
-    <div class="attraction-details">
-        <p>Our camp sites are designed to provide the ultimate outdoor experience, with spacious and comfortable accommodations that make you feel right at home in nature. Each site is carefully maintained and thoughtfully equipped with all the amenities you need to enjoy your stay, from fire pits for roasting marshmallows to picnic tables for sharing meals with loved ones. Whether you're a seasoned camper or new to the outdoors, our camp sites offer the perfect balance of comfort and adventure, making for an unforgettable experience you'll treasure for years to come.</p>
-        <button>Learn More</button>
-    </div>
-</div>
-
-<div class="attraction">
-    <div class="attraction-img">
-        <img src="https://images.pexels.com/photos/4818708/pexels-photo-4818708.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of wildlife">
+    <div class="attraction-img four">
+        <img src="https://images.pexels.com/photos/4818708/pexels-photo-4818708.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image of a bird">
     </div>
     <div class="attraction-details">
         <p>Our camp site is nestled in the heart of nature, surrounded by an abundance of wildlife that makes for a truly immersive and unforgettable experience. From majestic birds soaring overhead to playful deer grazing nearby, there's no shortage of opportunities to witness the wonders of nature up close. Whether you're an avid birdwatcher or just looking to connect with the natural world, the wildlife around our camp site is sure to leave a lasting impression.</p>
