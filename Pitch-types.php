16a2eca43f92d6eca5b0aeabc0fbcd94c927b310
KhumboMunsaka <khumbolane11@gmail.com>
2023-03-28 18:47:06 +0200

    made the footer work

--- Pitch-types.php
+++ Pitch-types.php
@@ -1,8 +1,8 @@
 <?php include 'nav.php'; ?>
-
 <main>
-    <div class="container">
+    <div class="container search-page">
         <div class="search">
+            <h1>f hdskfhsadklfjklsdfjkldsafj;askldjflkadsfa</h1>
             <input type="search" name="search" id="search">
             <button type="submit">Search for Available Pitches!</button>
         </div>
