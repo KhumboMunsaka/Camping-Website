671a9745fd08f710f58dcf9281fc548cede5e7ca
KhumboMunsaka <khumbolane11@gmail.com>
2023-03-28 16:41:30 +0200

    styling the information page

--- Information.php
+++ Information.php
@@ -1,2 +1,49 @@
 <?php include 'nav.php'; ?>
+<main> 
+    <div class="container pitches-content">
+        <h2>CampSites Available!</h2>
+        <div class="pitch-types">
+<div class="pitch-type">
+    <div class="pitch-type-img pitch-one">
+        <img src="https://images.pexels.com/photos/7967373/pexels-photo-7967373.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of a lake">
+    </div>
+    <div class="pitch-type-details">
+        <p>Our caravan pitches are suitable for all types of caravans, motorhomes, and campervans. They are equipped with electrical hook-ups, water supplies and drainage facilities, ensuring that your stay is comfortable and convenient. You can explore the area and then come back to your comfortable caravan.</p>
+                <button type="submit">Book Now</button>
+
+    </div>
+</div>
+
+
+<div class="pitch-type">
+    <div class="pitch-type-img pitch-two">
+       <img src="https://images.pexels.com/photos/3568479/pexels-photo-3568479.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of a campsite">
+    </div>
+    <div class="pitch-type-details ">
+        <p>Our seasonal pitches are perfect for those who want to enjoy the camping season to the fullest. You can stay for the entire season, enjoying the changing seasons and natural surroundings. With your own pitch, you can make the most of your time and enjoy the benefits of a longer stay.</p>
+  <button type="submit">Book Now</button>
+    </div>
+</div>
+
+<div class="pitch-type">
+    <div class="pitch-type-img pitch-three">
+        <img src="https://images.pexels.com/photos/939723/pexels-photo-939723.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of a biker">
+    </div>
+    <div class="pitch-type-details">
+        <p>Our tent pitches are perfect for those who want to experience the great outdoors in a traditional way. Each pitch is marked out with plenty of space for your tent and camping equipment. You can enjoy the natural surroundings and wake up to the sounds of birdsong and fresh air.</p>
+        <button type="submit">Book Now</button>
+    </div>
+</div>
+
+<div class="pitch-type">
+    <div class="pitch-type-img pitch-four">
+        <img src="https://images.pexels.com/photos/730426/pexels-photo-730426.jpeg?auto=compress&cs=tinysrgb&w=600" alt="image of a bird">
+    </div>
+    <div class="pitch-type-details">
+        <p>Our group pitches are ideal for families and friends who want to camp together. These pitches are larger, with plenty of space for multiple tents or caravans. You can spend time together, enjoying activities and meals in the great outdoors.</p>
+<button type="submit">Book Now</button>
+    </div>
+</div>
+
+</main>
 <?php include 'footer.php'; ?>
--- Pitch-types.php
+++ Pitch-types.php
@@ -1,2 +1,11 @@
 <?php include 'nav.php'; ?>
+
+<main>
+    <div class="container">
+        <div class="search">
+            <input type="search" name="search" id="search">
+            <button type="submit">Search for Available Pitches!</button>
+        </div>
+    </div>
+</main>
 <?php include 'footer.php'; ?>
