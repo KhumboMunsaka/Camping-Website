b85f3689911f8008cebce5d5262f3548147c8383
KhumboMunsaka <khumbolane11@gmail.com>
2023-05-02 16:23:41 +0200

    Done up some changes

--- Home.php
+++ Home.php
@@ -1,5 +1,29 @@
 <?php include 'nav.php'; ?>
-      <header aria-label="Newest Photos">
+<header>
+  <div class="bg-img" id="bg-img">
+    <div class="intro-heading">
+      <h1>GLOBAL WILD <span>SWIMMING</span> AND <span>CAMPING</span></h1>
+      <p>Experience the ultimate outdoor adventure with our top-quality camping and swimming facilities</p>
+    </div>
+    <div class="cta">
+      <button id="cta1">BOOK PITCH NOW</button>
+      <button id="cta2">BOOK POOL NOW</button>
+      <label for="search">Search</label>
+      <input type="search" name="search" id="search" placeholder="Search for available bookings!">
+    </div>
+  </div>
+</header>
+
+
+
+
+
+
+
+
+
+
+      <section aria-label="Newest Photos">
     <div class="carousel" data-carousel>
       <button class="carousel-button prev" data-carousel-button="prev">&#8678;</button>
       <button class="carousel-button next" data-carousel-button="next">&#8680;</button>
@@ -15,7 +39,13 @@
         </li>
       </ul>
     </div>
-      </header>
+    <section id="location">
+      <div class="location">
+        <h2>Find us here</h2>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d239.1326728978781!2d0.44236238401519784!3d50.92767274922418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2szm!4v1683033245013!5m2!1sen!2szm" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </section>
+      </section>
       <main>
         <main class="main-content">
           <div class="container">
