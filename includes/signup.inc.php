64e7985973dd1580f3c7243abf303dd21bd522f0
KhumboMunsaka <khumbolane11@gmail.com>
2023-05-03 01:08:16 +0200

    Trying out what Higgins from discord said

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,35 +1,32 @@
-<?php 
+<?php
 if (isset($_POST["submit"])) {
 
-$firstname =$_POST["firstname"];
-$lastname =$_POST["lastname"];
-$email =$_POST["email"];
-$number =$_POST["phone_number"];
-$password =$_POST["password"];
+    $email     = $_POST["email"];
+    $firstname = $_POST["firstname"];
+    $lastname  = $_POST["lastname"];
+    $number    = $_POST["phone_number"];
+    $password  = $_POST["password"];
 
-require_once 'dbh.inc.php';
-require_once 'functions.php';
+    require_once 'dbh.inc.php';
+    require_once 'functions.php';
 
-if (emptyInputSignup($name, $lastname, $email, $number, $password) !==false) {
-    header("location: ../sign-up.php?error=emptyinput");
-   exit();
-}
-if (invalidEmail($email) !==false) {
-    header("location: ../sign-up.php?error=invalidemail");
-   exit();
-}
-// if (invalidNumber($number) !==false) {
-//     header("location: ../sign-up.php?error=invalidnumber");
-//    exit();
-// }
-if (emailExists($conn, $email) !==false) {
-    header("location: ../sign-up.php?error=emailaddressalreadyexists!");
-   exit();
-}
+    if (emptyInputSignup($name, $lastname, $email, $number, $password) !== false) {
+        header("location: ../sign-up.php?error=emptyinput");
+        exit();
+    }
+    if (invalidEmail($email) !== false) {
+        header("location: ../sign-up.php?error=invalidemail");
+        exit();
+    }
+    // if (invalidNumber($number) !==false) {
+    //     header("location: ../sign-up.php?error=invalidnumber");
+    //    exit();
+    // }
+    if (emailExists($conn, $email) !== false) {
+        header("location: ../sign-up.php?error=emailaddressalreadyexists!");
+        exit();
+    }
 
-} else {
-    // header("location: ../sign-up.php");
-   exit();
+    // everything is good? lets create the user
+    createUser($conn, $firstname, $lastname, $email, $number, $password);
 }
-
-createUser($conn, $firstname, $lastname, $email, $number, $password);
