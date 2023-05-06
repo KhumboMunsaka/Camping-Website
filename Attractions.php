bfb0095b174b8a96e0f06f3724a802ed342a11c8
KhumboMunsaka <khumbolane11@gmail.com>
2023-05-06 03:57:56 +0200

    adding swimming

--- Attractions.php
+++ Attractions.php
@@ -3,6 +3,63 @@
     <div class="container attractions-content">
         <h2>What To See At Our CampSites</h2>
         <div class="attractions">
+<?php 
+if(isset($_POST['book_swim'])) {
+    $lake_name = $_POST['lake_name'];
+    $booking_date = $_POST['date'];
+    $email = $_SESSION['email'];
+
+    $select_checkout = mysqli_query($conn, "select * from `swim_sessions` where lake_name = '$lake_name'");
+
+    if(mysqli_num_rows($select_checkout) > 0) {
+      $message[] = 'This pitch is already booked. Please try later';
+    } else {
+        $insert_pitch = mysqli_query($conn, "insert into `pitch_bookings`(Pitch_name, Price, booking_date, email) values('$Pitch_name', '$price','$booking_date','$email')");
+        $message[] = 'This Pitch has been booked Successfully';
+    }
+}
+?>
+   
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '<div class="message"><span>'.$message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
+   };
+};
+?>
+ <div class="container pitches-content">
+        <?php 
+            $sql = 'Select * from lakes';
+            $result = mysqli_query($conn, $sql);
+            $lakes = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        ?>
+        <h2>Swim Sessions Available!</h2>
+        <?php foreach($lakes as $item): ?>      
+        <form action="" method="POST" >  
+            <div class="pitch-type">    
+                <div class="pitch-type-img pitch-one">
+                    <img src="<?php echo $item['image_link']?>" alt="image of a lake">
+                </div>
+                <div class="pitch-type-details">
+                    <input type="hidden" name="lake_name" value="<?php echo $item['lake_name'] ?>">
+                    <?php 
+                    echo $item['description'];
+                    if (isset($_SESSION['UserID']) || isset($_SESSION['email'])) {
+                        echo '
+                        <p>Set a date you would like to book</p>
+                        <input type="datetime-local" name="datetime-local" id="">
+                        <input type="submit" class="btn" value="Book Session" name="book_swim">';
+                    } else {
+                        echo '<p class="notLogged">You must<a href="sign-in.php"> sign in </a> to  book a swim session</p>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </form>
+        <?php endforeach; ?>
+    </div> 
+
 <div class="attraction">
     <div class="attraction-img one">
         <img src="https://images.pexels.com/photos/5620427/pexels-photo-5620427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image of a lake">
--- Pitch-types.php
+++ Pitch-types.php
@@ -5,7 +5,7 @@
             <form action="" method="POST">
                 <h1>Search Availability</h1>
                  <h2>CampSites Available!</h2>
-                <p>Click search to see which pitces are available</p>
+                <p>Click search to see which pitces or swim sessions are available</p>
                 <button type="submit" name="search">SEARCH</button>
             </form>
         </div>
@@ -42,8 +42,44 @@
                 </form>
                 <?php endforeach; ?>
             <?php } else { ?>
+                
                
-            <?php } ?>
+            <?php } 
+             if(isset($_POST['search'])) {
+                $sql = 'SELECT * FROM lakes LEFT JOIN swim_sessions ON lakes.lake_name = swim_sessions.lake_name WHERE lakes.lake_name IS NULL;';
+                $result = mysqli_query($conn, $sql);
+                $lakes = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                foreach($lakes as $item): ?>      
+                <form action="" method="POST" >  
+                    <div class="pitch-type">    
+                        <div class="pitch-type-img pitch-one">
+                            <img src="<?php echo $item['image_link']?>" alt="image of a lake">
+                        </div>
+                        <div class="pitch-type-details">
+                            <input type="hidden" name="Pitch_name" value="<?php echo $item['lake_name'] ?>">
+                            <input type="hidden" name="Price" value="<?php echo $item['Price'] ?>">
+                            <?php 
+                            echo $item['Description'];
+                            if (isset($_SESSION['UserID']) || isset($_SESSION['email'])) {
+                                echo '
+                                <p>Set a date you would like to book</p>
+                                <input type="date" name="date" id="date">
+                                <input type="submit" class="btn" value="Book Pitch" name="book_swim">';
+                            } else {
+                                echo '<p class="notLogged">You must<a href="sign-in.php"> sign in </a> to  book a pitch</p>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </form>
+                <?php endforeach; ?>
+            <?php } else { ?>
+                
+               
+            <?php } 
+        
+            
+            ?>
     </div> 
 </main>
 <?php include 'footer.php'; ?>
